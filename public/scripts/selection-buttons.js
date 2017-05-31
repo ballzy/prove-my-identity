999fc108317384d8fa1475e38a979f792c48ed2b
nishantverma160380 <nishantverma1@gmail.com>
2017-05-31 14:28:46 +0100

    New script code to navigate different pages using button value.

--- public/scripts/selection-buttons.js
+++ public/scripts/selection-buttons.js
@@ -120,3 +120,8 @@ $(document).ready(function() {
   new GOVUK.SelectionButtons($blockLabels);
 
 });
+
+$( "#continue" ).click(function() {
+    var value = $("input:radio[name=radio-inline-group]:checked").val();
+        location.href = value;
+});
